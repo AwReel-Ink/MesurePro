e6883ebba129b43e73b25fff01d054793d1b2a97
AwReel-Ink <awreeldroid@gmail.com>
2026-05-02 21:06:09 +0200

    Update sw.js1.2

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ================================================================
 // SERVICE WORKER — MesurePro
 // ================================================================
-const CACHE_NAME = 'mesurepro-v1';
+const CACHE_NAME = 'mesurepro-v1.2';
 const ASSETS = [
   './',
   './index.html',
